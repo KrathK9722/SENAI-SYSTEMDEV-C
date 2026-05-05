cc4da61bf92ccaf423b93eda68b0e42a71fd32a1
Krath <arthurkochangasther@gmail.com>
2026-05-05 18:55:23 -0300

    d

--- Activitie #5 - Show Number/par.c
+++ Activitie #5 - Show Number/par.c
@@ -1,1 +1,25 @@
 #include <stdio.h>
+#include <string.h>
+#include <windows.h>
+
+int main(void){
+    SetConsoleOutputCP(65001);
+    SetConsoleCP(65001);
+
+    int number;
+    char parOuImpar[30] = "é impar", negativoOuPositivo[30] = "é positivo";
+    
+    printf("Verifique se seu número é negativo/positivo e par/ímpar.");
+    printf("\n\nEscreva seu número: ");
+    scanf("%d", &number);
+
+    if (number % 2 == 0){
+        strcpy(parOuImpar, "é par");
+    }
+    if (number < 0){
+        strcpy(negativoOuPositivo, "é negativo");
+    }
+    
+    printf("O número %d %s e %s",number,parOuImpar,negativoOuPositivo);
+    
+}
