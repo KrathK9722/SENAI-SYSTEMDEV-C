d695912d97c23b7b52bc8969fa4adaf5e300567c
Krath <arthurkochangasther@gmail.com>
2026-05-04 21:20:24 -0300

    Release new codes

---
+++ Activitie #6 -  Product Discount/desconto.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main(void){
+    SetConsoleCP(65001);
+    SetConsoleOutputCP(65001);
+
+    float preco, valorFinal;
+    int quantidade;
+
+    printf("\nDigite o valor do produto: ");
+    scanf("%f",&preco);
+    printf("Digite a quantidade de itens: ");
+    scanf("%d",&quantidade);
+    valorFinal = quantidade * preco;
+    if(valorFinal >=100){
+        valorFinal = valorFinal * 0.95;
+        printf("Valor da sua compra ficou: R$%.2f com 5%% de desconto.",valorFinal);
+    }
+    else{
+        printf("Valor da sua compra ficou: R$%.2f",valorFinal);
+    }
+    
+}
