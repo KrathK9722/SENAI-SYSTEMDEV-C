2c4d7a3ee3f6d2d9fd3410149efc757e4e31966d
Krath <arthurkochangasther@gmail.com>
2026-05-11 19:52:21 -0300

    Activitie #14

---
+++ Activitie #14 - Find Number in Vector/vector_6.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main(void){
+    SetConsoleOutputCP(65001);
+    int numero[8] = {15, 42, 8, 73, 29, 61, 4, 37},numeroDigitado,pos;
+    
+    //Procurar número digitado
+    printf("Qual número quer procurar:");
+    scanf("%d",&numeroDigitado);
+
+    for(int i=0;i<8;i++){
+        if(numero[i] == numeroDigitado){
+            printf("Número encontrado na posição %dª.",i+1);
+            break;
+        }
+        else if(i==7){
+            printf("Número não foi encontrado");
+        }
+    }
+}
